c1d1b3266919ce12671857ca5a7262f3cb36f4bf
ngarukaalx <hiramkabuagi@gmail.com>
2023-12-08 13:20:02 +0000

    sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,9 @@
 # Prepare your web servers
 
 # install Nginx if it not already installed
-if [ ! -x "$(command -v nginx)" ]; then
-	apt-get update
-	apt-get install -y nginx
-fi
+sudo apt-get update
+sudo apt-get update
+sudo apt-get install -y nginx
 
 # create the folder /data/ if it doest exit
 sudo mkdir -p /data/web_static/releases/test /data/web_static
@@ -17,14 +16,14 @@ echo "Hello hiram!" | tee -a /data/web_static/releases/test/index.html
 release_folder="/data/web_static/releases/"
 current_link="/data/web_static/current"
 # create a new symbolick link
-ln -sf "$release_folder/test" "$current_link"
+sudo ln -sf "$release_folder/test" "$current_link"
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
-chown -hR ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 nginx_config="/etc/nginx/sites-enabled/default"
 sudo sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $nginx_config
 
-nginx -t
-service nginx restart
+sudo nginx -t
+sudo service nginx restart
