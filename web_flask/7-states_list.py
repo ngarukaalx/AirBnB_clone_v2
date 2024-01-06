f91c982bbae0b0518d53ea2d8a483340b47b4016
ngarukaalx <hiramkabuagi@gmail.com>
2024-01-06 14:31:29 +0000

    lambda

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,11 +2,9 @@
 """ starts a Flask web application with specified requirements
 """
 
-import sys
 from flask import Flask, render_template
-from models import storage
-from models.state import State
-sys.path.append("..")
+import models
+from models.state import State, storage
 
 app = Flask(__name__)
 
@@ -20,9 +18,10 @@ def teardown_appcontext(exception):
 @app.route("/states_list", strict_slashes=False)
 def state_display():
     """ display a HTML page with list of state object """
-    statess = storage.all(State).values()
+    states = sorted(list(models.storage.all(State).values()),
+                    key=lambda state: state["name"])
 
-    return render_template("7-states_list.html", states=statess)
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
